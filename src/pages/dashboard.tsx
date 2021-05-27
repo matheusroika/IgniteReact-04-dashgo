90d868fd4139d881714881983ec4e16f0537538d
Matheus Roika <matheusroika@hotmail.com>
2021-05-27 17:05:12 -0300

    Página: Dashboard Usando a biblioteca ApexCharts, colocamos em tela e configuramos a estilização dos gráficos que teremos na nossa Dashboard. Nessa aula, aprendemos sobre a função 'dynamic' do Next, que nos permite fazer lazy loading e desabilitar o Server-Side Rendering para os elementos que quisermos.

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,6 +1,66 @@
-import { Flex } from "@chakra-ui/react";
+import { Box, Flex, SimpleGrid, Text, theme } from "@chakra-ui/react";
+import dynamic from 'next/dynamic'
 import { Header } from "../components/Header";
 import { Sidebar } from "../components/Sidebar";
+
+import ptBR from 'apexcharts/dist/locales/pt-br.json'
+const Chart = dynamic(() => import('react-apexcharts'), {
+  ssr: false,
+})
+
+const chartOptions = {
+  chart: {
+    defaultLocale: 'pt-br',
+    locales: [ptBR],
+    toolbar: {
+      show: false,
+    },
+    zoom: {
+      enabled: false,
+    },
+    foreColor: theme.colors.gray[500],
+  },
+  grid: {
+    show: false,
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  tooltip: {
+    enabled: false,
+  },
+  xaxis: {
+    type: 'datetime' as 'datetime',
+    axisBorder: {
+      color: theme.colors.gray[600]
+    },
+    axisTicks: {
+      color: theme.colors.gray[600]
+    },
+    categories: [
+      '2021=05=23T00:00:00.000Z',
+      '2021=05=24T00:00:00.000Z',
+      '2021=05=25T00:00:00.000Z',
+      '2021=05=26T00:00:00.000Z',
+      '2021=05=27T00:00:00.000Z',
+      '2021=05=28T00:00:00.000Z',
+      '2021=05=29T00:00:00.000Z',
+    ]
+  },
+  fill: {
+    opacity: 0.3,
+    type: 'gradient',
+    gradient: {
+      shade: 'dark',
+      opacityFrom: 0.7,
+      opacityTo: 0.3,
+    }
+  },
+}
+
+const chartSeries = [
+  {name: 'series1', data: [31, 120, 10, 28, 51, 18, 109]}
+]
 
 export default function Dashboard() {
   return (
@@ -9,6 +69,19 @@ export default function Dashboard() {
 
       <Flex w='100%' maxW={1480} my='6' mx='auto' px='6' >
         <Sidebar />
+
+        <SimpleGrid flex='1' gap='4' minChildWidth={320} align='flex-start'>
+          <Box p='8' bg='gray.800' borderRadius={8} pb='4'>
+            <Text fontSize='lg' mb='4'>Inscritos da semana</Text>
+            <Chart options={chartOptions} series={chartSeries} type='area' height={160} />
+          </Box>
+
+          <Box p='8' bg='gray.800' borderRadius={8} pb='4'>
+            <Text fontSize='lg' mb='4'>Taxa de abertura</Text>
+            <Chart options={chartOptions} series={chartSeries} type='area' height={160} />
+          </Box>
+
+        </SimpleGrid>
       </Flex>
     </Flex>
   )

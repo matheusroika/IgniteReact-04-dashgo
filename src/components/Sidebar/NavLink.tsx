46581b8dfa2b8720b21c9b7696e7f4246222c266
Matheus Roika <matheusroika@hotmail.com>
2021-05-28 18:46:18 -0300

    Sinalizando link ativo

--- src/components/Sidebar/NavLink.tsx
+++ src/components/Sidebar/NavLink.tsx
@@ -1,6 +1,7 @@
 import { Icon, Link as ChakraLink, Text } from "@chakra-ui/react";
 import { IconType } from "react-icons";
 import Link from 'next/link'
+import { useRouter } from "next/router";
 
 interface NavLinkProps {
   icon: IconType;
@@ -9,9 +10,12 @@ interface NavLinkProps {
 }
 
 export function NavLink({ icon, text, href }: NavLinkProps) {
+  const { asPath } = useRouter()
+  const isActive = asPath === '/' ? false : asPath.startsWith(href)
+  
   return (
     <Link href={href} passHref>
-      <ChakraLink display='flex' align='center' >
+      <ChakraLink color={isActive ? 'pink.400' : 'gray.50'} display='flex' align='center' >
         <Icon as={icon} fontSize='20' />
         <Text ml='4' fontWeight='medium'>{text}</Text>
       </ChakraLink>

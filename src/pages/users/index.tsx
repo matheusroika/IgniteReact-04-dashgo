ac0ea433dedfcf073bc8fdf634ba1ff6052d888b
Matheus Roika <matheusroika@hotmail.com>
2021-06-03 16:37:01 -0300

    Listagem de usuários

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -12,7 +12,20 @@ export default function UserList() {
     const response = await fetch('http://localhost:3000/api/users')
     const data = await response.json()
 
-    return data
+    const users = data.users.map(user => {
+      return {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric',
+        })
+      }
+    })
+
+    return users
   })
 
   const isWideScreen = useBreakpointValue({
@@ -79,39 +92,41 @@ export default function UserList() {
                   </Tr>
                 </Thead>
                 <Tbody>
-                  <Tr>
-                    <Td px={['0', '4', '6']}>
-                      <Checkbox colorScheme='pink' />
-                    </Td>
-                    <Td>
-                      <Box>
-                        <Text fontWeight='bold'>Matheus Roika</Text>
-                        <Text fontSize='sm' color='gray.300'>matheus@roika.design</Text>
-                      </Box>
-                    </Td>
-                    { isWideScreen && <Td>04 de Abril, 2021</Td> }
-                    <Td px={['0', '4', '6']} textAlign="end">
-                      { isWideScreen ? (
-                        <Button
-                          as="a"
-                          size='sm'
-                          fontSize='sm'
-                          colorScheme='purple'
-                          leftIcon={<Icon as={RiPencilLine} fontSize='16'/>}
-                        >
-                          Editar
-                        </Button>) : (
-                        <IconButton
-                          aria-label="Editar"
-                          icon={<Icon as={RiPencilLine} fontSize='16' />}
-                          as="a"
-                          size='sm'
-                          fontSize='sm'
-                          colorScheme='purple'
-                        />)
-                      }
-                    </Td>
-                  </Tr>
+                  { data.map(user => (
+                    <Tr key={user.id}>
+                      <Td px={['0', '4', '6']}>
+                        <Checkbox colorScheme='pink' />
+                      </Td>
+                      <Td>
+                        <Box>
+                          <Text fontWeight='bold'>{ user.name }</Text>
+                          <Text fontSize='sm' color='gray.300'>{ user.email }</Text>
+                        </Box>
+                      </Td>
+                      { isWideScreen && <Td>{ user.createdAt }</Td> }
+                      <Td px={['0', '4', '6']} textAlign="end">
+                        { isWideScreen ? (
+                          <Button
+                            as="a"
+                            size='sm'
+                            fontSize='sm'
+                            colorScheme='purple'
+                            leftIcon={<Icon as={RiPencilLine} fontSize='16'/>}
+                          >
+                            Editar
+                          </Button>) : (
+                          <IconButton
+                            aria-label="Editar"
+                            icon={<Icon as={RiPencilLine} fontSize='16' />}
+                            as="a"
+                            size='sm'
+                            fontSize='sm'
+                            colorScheme='purple'
+                          />)
+                        }
+                      </Td>
+                    </Tr>
+                  )) }
                 </Tbody>
               </Table>
     

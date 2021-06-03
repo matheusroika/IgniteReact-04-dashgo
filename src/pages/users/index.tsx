d0b0d07c083c679053a1b0690138db3f939c4678
Matheus Roika <matheusroika@hotmail.com>
2021-06-03 17:15:16 -0300

    Criando hook de listagem de usuários

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,8 +1,7 @@
 import Link from "next/link";
 import { Box, Button, Center, Checkbox, Flex, Heading, Icon, IconButton, Spinner, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
-import { useQuery } from 'react-query'
 
-import { api } from "../../services/api";
+import { useUsers } from "../../services/hooks/useUsers";
 
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
@@ -10,26 +9,7 @@ import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
-  const { data, isLoading, isFetching, error } = useQuery('users', async () => {
-    const { data } = await api.get('users')
-
-    const users = data.users.map(user => {
-      return {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
-          day: '2-digit',
-          month: 'long',
-          year: 'numeric',
-        })
-      }
-    })
-
-    return users
-  }, {
-    staleTime: 1000 * 5, //5 seconds
-  })
+  const { data, isLoading, isFetching, error } = useUsers()
 
   const isWideScreen = useBreakpointValue({
     base: false,
